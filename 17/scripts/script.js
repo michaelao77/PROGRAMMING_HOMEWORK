af2e449a920dbf39a952e95df2d08a1c2cd01f37
michaelao77 <31858611+michaelao77@users.noreply.github.com>
2017-12-07 16:49:51 -0500

    Add files via upload

---
+++ 17/scripts/script.js
@@ -0,0 +1,150 @@
+// Day/Night Toggle
+function changeBackground() {
+   document.getElementById("grid").style.background = "#242424";
+}
+
+
+function changeBackgroundBack() {
+   document.getElementById("grid").style.background = "white";
+   document.getElementById("footer").style.background = "#f8f8f5";
+}
+
+
+// Functions showing/hiding individual overlays
+
+// Chicken
+function chicken() {
+    document.getElementById("chicken").style.display = "block";
+}
+
+function noChicken() {
+    document.getElementById("chicken").style.display = "none";
+}
+
+// Avocado
+function avocado() {
+	document.getElementById("avocado").style.display = "block";
+}
+function noAvocado() {
+    document.getElementById("avocado").style.display = "none";
+}
+
+// Donut
+function donut() {
+	document.getElementById("donut").style.display = "block";
+}
+function noDonut() {
+    document.getElementById("donut").style.display = "none";
+}
+
+// Sun
+function sun() {
+	document.getElementById("sun").style.display = "block";
+}
+function noSun() {
+    document.getElementById("sun").style.display = "none";
+}
+
+// Diamond
+function diamond() {
+	document.getElementById("diamond").style.display = "block";
+}
+function noDiamond() {
+    document.getElementById("diamond").style.display = "none";
+}
+
+// Shark
+function shark() {
+	document.getElementById("shark").style.display = "block";
+}
+function noShark() {
+    document.getElementById("shark").style.display = "none";
+}
+
+// Skull
+function skull() {
+	document.getElementById("skull").style.display = "block";
+}
+function noSkull() {
+    document.getElementById("skull").style.display = "none";
+}
+
+// Koi
+function koi() {
+	document.getElementById("koi").style.display = "block";
+}
+function noKoi() {
+    document.getElementById("koi").style.display = "none";
+}
+
+// Tongue
+function tongue() {
+	document.getElementById("tongue").style.display = "block";
+}
+function noTongue() {
+    document.getElementById("tongue").style.display = "none";
+}
+
+// Sumo
+function sumo() {
+	document.getElementById("sumo").style.display = "block";
+}
+function noSumo() {
+    document.getElementById("sumo").style.display = "none";
+}
+
+// Dove
+function dove() {
+	document.getElementById("dove").style.display = "block";
+}
+function noDove() {
+    document.getElementById("dove").style.display = "none";
+}
+
+// Aquafresh
+function aquafresh() {
+	document.getElementById("aquafresh").style.display = "block";
+}
+function noAquafresh() {
+    document.getElementById("aquafresh").style.display = "none";
+}
+
+// Nikes
+function nikes() {
+	document.getElementById("nikes").style.display = "block";
+}
+function noNikes() {
+    document.getElementById("nikes").style.display = "none";
+}
+
+// iPad
+function ipad() {
+	document.getElementById("ipad").style.display = "block";
+}
+function noIpad() {
+    document.getElementById("ipad").style.display = "none";
+}
+
+// yinyang
+function yinyang() {
+	document.getElementById("yinyang").style.display = "block";
+}
+function noYinyang() {
+    document.getElementById("yinyang").style.display = "none";
+}
+
+// Dollar
+function dollar() {
+	document.getElementById("dollar").style.display = "block";
+}
+function noDollar() {
+    document.getElementById("dollar").style.display = "none";
+}
+
+// Beetle
+function beetle() {
+	document.getElementById("beetle").style.display = "block";
+}
+function noBeetle() {
+    document.getElementById("beetle").style.display = "none";
+}
